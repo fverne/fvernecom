f56ccca5e67f8d4d667c3b1bfbcd0e4d1c873d42
fverne <frederikverne95@gmail.com>
2022-03-14 19:31:40 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"> // Use 'className' instead of 'class'
+    <div className="App"> 
       <div className="navigation">
         <img src={logo} className="logo" alt="Logo Image" />
         <div className="navigation-sub">
